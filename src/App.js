4e31e3a704d97a645f4699a5bba7f07e82c7fb14
JulianPerez852 <jdph852@gmail.com>
2019-06-12 19:54:23 -0500

    commit para la prueba

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      contador:0,
+    }
+    this.onChangeText=this.onChangeText.bind(this);
+  }
+
+  onChangeText(e){
+    const conteo = e.target.value.length
+    this.setState({
+      contador: conteo 
+    })
+  }
+
   render() {
+    const { contador }= this.state
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={(value)=>this.onChangeText(value)} ></textarea>
+        <div className="counter">{contador}</div>
       </div>
     );
   }
